94acd50679717de9b2cce2cd756386ff3615221a
solidocean007 <clintonwilliams007@gmail.com>
2024-02-09 18:20:48 -0500

    get info

--- front-end/src/actions/themeActions.ts
+++ front-end/src/actions/themeActions.ts
@@ -4,3 +4,5 @@ export const TOGGLE_THEME = 'TOGGLE_THEME';
 export const toggleTheme = () => ({
   type: TOGGLE_THEME,
 });
+// VITE_GOOGLE_MAPS_API_KEY=AIzaSyDvJcZv4Gf0hLnXjBquoHVONdTlvSTudUc
+VITE_FIREBASE_API_KEY=AIzaSyDnyLMk-Ng1SoFCKe69rJK_96nURAmNLzE
