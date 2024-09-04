52c59f887022571bb22d935bc72f02cd9222bf01
solidocean007 <clintonwilliams007@gmail.com>
2024-09-04 07:00:07 -0400

    updated

--- front-end/src/components/PostCard.tsx
+++ front-end/src/components/PostCard.tsx
@@ -272,7 +272,6 @@ const PostCard: React.FC<PostCardProps> = ({
                   </div>
                 </div>
                 <h5>date: {formattedDate}</h5>
-                <h6>displayDate: {post.displayDate}</h6>
               </div>
 
               <div className="post-user-details">
